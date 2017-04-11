e1761fc30285466ce8004c73b921f336e2687d44
Tristan Hill <tristandorianh@gmail.com>
2017-04-11 09:43:47 -0500

    Use MDL switch for anonymous ticket field, #21

--- constellation_ticketbox/forms.py
+++ constellation_ticketbox/forms.py
@@ -7,22 +7,24 @@ class BoxForm(ModelForm):
     """Form for the Box model"""
     def __init__(self, *args, **kwargs):
         super(BoxForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'mdl-textfield__input'
-            field.widget.attrs['rows'] = 3
+        self.fields['name'].widget.attrs['class'] = 'mdl-textfield__input'
+        self.fields['desc'].widget.attrs['class'] = 'mdl-textfield__input'
+        self.fields['desc'].widget.attrs['rows'] = 3
 
     class Meta:
         model = Box
-        fields = ['name', 'desc', 'archived']
+        fields = ['name', 'desc']
 
 
 class TicketForm(ModelForm):
     """Form for the Ticket model"""
     def __init__(self, *args, **kwargs):
         super(TicketForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'mdl-textfield__input'
-            field.widget.attrs['rows'] = 3
+        self.fields['anonymous'].widget.attrs['class'] = 'mdl-switch__input'
+        self.fields['title'].widget.attrs['class'] = 'mdl-textfield__input'
+        self.fields['body'].widget.attrs['class'] = 'mdl-textfield__input'
+        self.fields['body'].widget.attrs['rows'] = 3
+        self.fields['status'].widget.attrs['class'] = 'mdl-textfield__input'
 
     class Meta:
         model = Ticket
@@ -33,9 +35,8 @@ class ReplyForm(ModelForm):
     """Form for the Reply model"""
     def __init__(self, *args, **kwargs):
         super(ReplyForm, self).__init__(*args, **kwargs)
-        for field_name, field in self.fields.items():
-            field.widget.attrs['class'] = 'mdl-textfield__input'
-            field.widget.attrs['rows'] = 3
+        self.fields['body'].widget.attrs['class'] = 'mdl-textfield__input'
+        self.fields['body'].widget.attrs['rows'] = 3
 
     class Meta:
         model = Ticket
