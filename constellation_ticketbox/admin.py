b2157c99658054bc79ffefe33bd37508f96f7e33
Tristan Hill <tristandorianh@gmail.com>
2017-04-11 23:51:34 -0500

    Remove model registration, #20

--- constellation_ticketbox/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-
-from .models import Box
-from .models import Ticket
-from .models import Reply
-
-admin.site.register(Box)
-admin.site.register(Ticket)
-admin.site.register(Reply)
